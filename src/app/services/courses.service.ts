da392b382f701d45fdc3f2ed1b9ad9bf000dfc47
orzbirou <orzbirou@users.noreply.github.com>
2025-12-29 13:23:17 +0200

    Add shareReplay operator to loadedAllCourses method
    - Added shareReplay() operator to cache HTTP response and prevent multiple API calls
    - Reformatted pipe chain for better readability

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, shareReplay } from 'rxjs/operators';
 import { Course } from '../model/course';
 
 @Injectable({
@@ -12,8 +12,10 @@ export class CoursesService {
   constructor( private http: HttpClient) { }
 
   loadedAllCourses() : Observable<Course[]> {
-    return this.http.get<Course[]>('/api/courses').pipe(
-      map(res => res['payload'])
+    return this.http.get<Course[]>('/api/courses')
+    .pipe(
+      map(res => res['payload']),
+      shareReplay()
     );
   } 
 }
