772677f07d389c79c83a4875ce657738ab67037f
orzbirou <orzbirou@users.noreply.github.com>
2025-12-29 14:26:24 +0200

    Generate courses-card-list component and configure Angular CLI defaults
    - Created CoursesCardListComponent with template, styles, and tests
    - Added @Input() courses property to receive course data
    - Integrated MatDialog for opening course dialogs
    - Updated angular.json to set standalone: false as default for new components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,7 +21,7 @@ export class HomeComponent implements OnInit {
   advancedCourses$: Observable<Course[]>;
 
 
-  constructor(private coursesService: CoursesService, private dialog: MatDialog) {
+  constructor(private coursesService: CoursesService) {
 
   }
 
@@ -43,20 +43,6 @@ export class HomeComponent implements OnInit {
         courses => courses.filter(course => course.category == "ADVANCED")
       )
     );
-
-  }
-
-  editCourse(course: Course) {
-
-    const dialogConfig = new MatDialogConfig();
-
-    dialogConfig.disableClose = true;
-    dialogConfig.autoFocus = true;
-    dialogConfig.width = "400px";
-
-    dialogConfig.data = course;
-
-    const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
 
   }
 
